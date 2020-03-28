c77aa9a5f865e6107b332fa97c4f129fa28d73d3
Abhishek Surya <abhisheksurya@gatech.edu>
2020-03-28 14:24:46 +0100

    tdd setup

--- source/employee.py
+++ source/employee.py
@@ -1,3 +1,19 @@
+"""This class is a logical conception of Employee"""
 
-class Employee():
-    test_var = "Test"
+
+class Employee:
+
+    """
+    Description -
+    Constructor to initialize employee attributes
+    Parameters -
+    first_name: string, required
+    last_name: string, required
+    Returns -
+    NA
+    """
+
+    def __init__(self, first_name, last_name):
+        self.employee_first_name = first_name
+        self.employee_last_name = last_name
+        self.employee_email = first_name + "." + last_name + "@abhisheksurya.dev"
--- test/employee_test.py
+++ test/employee_test.py
@@ -1,10 +1,22 @@
 import unittest
+from source.employee import Employee
 
 
 class Employee_test(unittest.TestCase):
-    def test_email(self):
-        emp1 = "test@test.com"
-        self.assertEquals(emp1, "test@test.com")
+    def test_employee_details(self):
+        test_employee = Employee("Abhishek", "Surya")
+        self.assertEquals(test_employee.employee_first_name, "Abhishek")
+        self.assertEquals(test_employee.employee_last_name, "Surya")
+        self.assertEquals(
+            test_employee.employee_email, "Abhishek.Surya@abhisheksurya.dev"
+        )
+        self.assertNotEquals(
+            test_employee.employee_email, "AbhishekSurya@abhisheksurya.dev"
+        )
+
+    def test_employee_fullname(self):
+        test_employee = Employee("Abhishek", "Surya")
+        self.assertEqual(test_employee.get_fullname, "Surya, Abhishek")
 
 
 if __name__ == "__main__":
